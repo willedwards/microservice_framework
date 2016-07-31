63e1a551d1df2249dc23994fffb04fae507f91f0
willedwards <will.edwards@me.com>
2016-07-31 20:18:30 +0100

    reducing boilderplate code

--- event-sourcing/event-repository/event-repository-jdbc/src/main/java/uk/gov/justice/services/eventsourcing/repository/jdbc/eventlog/EventLog.java
+++ event-sourcing/event-repository/event-repository-jdbc/src/main/java/uk/gov/justice/services/eventsourcing/repository/jdbc/eventlog/EventLog.java
@@ -2,23 +2,21 @@ package uk.gov.justice.services.eventsourcing.repository.jdbc.eventlog;
 
 import lombok.*;
 
-import java.util.Objects;
 import java.util.UUID;
 
 /**
  * Entity class to represent a persisted event.
  */
-@Getter
-@AllArgsConstructor
+@Value
 @EqualsAndHashCode
-@ToString
+@ToString(includeFieldNames=true)
 public class EventLog {
 
-    private final UUID id;
-    private final UUID streamId;
-    private final Long sequenceId;
-    private final String name;
-    private final String payload;
-    private final String metadata;
+    private UUID id;
+    private UUID streamId;
+    private Long sequenceId;
+    private String name;
+    private String payload;
+    private String metadata;
 
 }
